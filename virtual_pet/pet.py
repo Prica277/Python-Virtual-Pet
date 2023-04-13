beb2d14fea5a74e1fd5227ff8e87042a4f3a4d41
Prica277 <prica277@hsd.k12.or.us>
2023-04-13 14:58:09 -0700

    finishing play function

--- virtual_pet/pet.py
+++ virtual_pet/pet.py
@@ -32,7 +32,7 @@ class Pet:
         """Let the user choose how to play with the pet."""
         menu = f"\nChoose how you would like to play with {self.name}:\n"
         menu += "\n\t1 - Fetch\n\t2 - Tug-Of-War"
-        menu += "\n\t3 - Stroke pet\n\t4 - Quit"
+        menu += "\n\t3 - Stroke Pet\n\t4 - Quit"
 
         choice = ""
         while choice != "4":
@@ -50,6 +50,32 @@ class Pet:
                     description += f"{self.name} drops the toy at your feet. "
                     description += f"{self.name} seems to really like this game!"
                     self.happiness += 2
+            elif choice == "2":
+                description = f"{self.name} gladly grabs the rope toy and "
+                description += "begins to pull on it."
+                if random.choice("01") == "1":
+                    description = f"{self.name} sucessfully pulls the rope toy "
+                    description += "out of your hands and runs away with it. "
+                    self.happiness += 1
+                    # quit since the pet ran off with the toy
+                    choice == "4"
+                else: 
+                    description = f"{self.name} loses its grip on the rope toy "
+                    description += "and falls over into your lap. "
+                    description += f"{self.name} seems to like this game!"
+                    self.happiness += 1
+            elif choice == "3":
+                description = f"You stroke {self.name}'s back."
+                if random.choice("01") == "1":
+                    description = f"{self.name} huffs and pulls away. It seems "
+                    description += "that it doesn't want to be petted right now."
+                    self.happiness -= 1
+                    # quit because the pet is upset
+                    choice == "4"
+                else: 
+                    description = f"{self.name} becomes a happy puddle on the "
+                    description += "floor. It likes being petted!"
+                    self.happiness += 1
             elif choice == "4":
                 description = f"{self.name} seems disappointed, but goes back to "
                 description += "their favorite spot. You are done playing."
@@ -66,6 +92,7 @@ class Pet:
                 print(description)
             # after play, provide update
             description += f"{self.name}'s happiness is at {self.happiness}"
+            
 
 # global scope
 if __name__ == "__main__":
--- virtual_pet/utilities.py
+++ virtual_pet/utilities.py
@@ -23,6 +23,10 @@ def get_menu_choice(menu: str, legal_choices: tuple) -> str:
     while user_choice not in legal_choices:
         print(menu)
         user_choice = input("Your Choice: ")
+        # Give feedback if user didn't use proper choice
+        if user_choice not in legal_choices:
+            print("Sorry, that is not an option. Please select one of the following: ")
+            print(legal_choices)
 
     return user_choice
 
@@ -30,3 +34,18 @@ if __name__ == "__main__":
     menu = "Here is your list of options:\n\t1 - Option #1\n"
     menu += "\t2 - Option #2\n\t3 - Option #3\n\n"
     selection = get_menu_choice(menu, ("1", "2", "3"))
+
+    print(f"\nYou selected {selection}")
+
+    new_menu = """
+    Here is your list of options: 
+    
+        1 - Option 1: Play with your pet.
+        2 - Option 2: Feed your pet
+        3 - Option 3:
+        4 - Option 4: 
+        """
+    
+    options = ("1", "2", "3", "4")
+    new_choice = get_menu_choice(new_menu, options)
+    print("You selected: " + new_choice)
