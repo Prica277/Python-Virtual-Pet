5fdf7d94cc0b22d2fb7f76f75b097b40a35dac5e
Prica277 <prica277@hsd.k12.or.us>
2023-05-26 12:58:36 -0700

    adding new functions

--- virtual_pet/main.py
+++ virtual_pet/main.py
@@ -1,4 +1,5 @@
 import utilities
+import json
 from possible_pets import Pet
 
 def main():
@@ -20,12 +21,41 @@ def main():
             pet = create_new_pet()
             active_pets.append(pet)
         elif choice == "2":
+            pet = get_pets()
+            get_active_pets(active_pets, pets)
+
             if not active_pets:
                 print("\nYou have no pets to play with.")
+                print("Try creating a new pet.")
+                break
+            current_pet = get_current_pet(active_pets)
+
         elif choice == "3":
             if active_pets:
                 for pet in active_pets:
                     pet.store_pet_data()
+
+def get_current_pet(pets):
+    """Show all pets and have user select which pet to interact with."""
+    # If there is only one active pet, return it
+    if len(pets) == 1:
+        return pets[0]
+    
+    choice = ""
+
+    #build menu
+    menu = "Which pet would you like to interact with?\n"
+    legal_choices = []
+    for i in range(len(pets)):
+        legal_choices.append(str(i))
+        menu += "\n" + str(i) + f"\t{pets[i]}"
+    while choice not in legal_choices:
+        choice = utilities.get_menu_choice(menu, tuple(legal_choices))
+    return pets[int(choice)]
+
+
+def get_active_pets(active_pets, pets):
+
 
 def create_new_pet():
     """ creates and returns a new pet object"""
